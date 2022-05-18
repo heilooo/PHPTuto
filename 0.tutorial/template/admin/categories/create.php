e6b1192f5929d33d1bb503880fc8c500f17de473
Abbas Dadvand <am.dadvand@gmail.com>
2022-05-18 15:43:13 +0430

    20220518

--- 0.tutorial/template/admin/categories/create.php
+++ 0.tutorial/template/admin/categories/create.php
@@ -1,25 +1,6 @@
 <?php
 require_once(realpath(dirname(__FILE__) . "/../layouts/head-tag.php"));
 ?>
-<<<<<<< HEAD
-
-
-
-            <section class="pt-3 pb-1 mb-2 border-bottom">
-                <h1 class="h5">Create Category</h1>
-            </section>
-            <section class="row my-3">
-                <section class="col-12">
-                    <form method="post" action="http://localhost/PHPTuto/0.tutorial/category/store">
-                        <div class="form-group">
-                            <label for="name">Title</label>
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter name ...">
-                        </div>
-                        <button type="submit" class="btn btn-primary btn-sm">store</button>
-                    </form>
-                </section>
-            </section>
-=======
     <section class="pt-3 pb-1 mb-2 border-bottom">
         <h1 class="h5">Create Category</h1>
     </section>
@@ -34,7 +15,6 @@ require_once(realpath(dirname(__FILE__) . "/../layouts/head-tag.php"));
             </form>
         </section>
     </section>
->>>>>>> main
 <?php
 require_once(realpath(dirname(__FILE__) . "/../layouts/footer.php"));
 ?>
