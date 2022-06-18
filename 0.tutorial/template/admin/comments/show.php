cc60dbaada4bd7352296f947d07ced9f993d7f81
abbasDadvand <am.dadvand@gmail.com>
2022-06-18 21:44:48 +0430

    0.13.0**67..

---
+++ 0.tutorial/template/admin/comments/show.php
@@ -0,0 +1,17 @@
+
+    <section class="pt-3 pb-1 mb-2 border-bottom">
+        <h1 class="h5">Show Comment</h1>
+    </section>
+<section class="row my-3">
+    <section class="col-12">
+        <h1 class="h4 border-bottom">6</h1>
+        <p class="text-secondary border-bottom">8</p>
+        <p class="text-secondary border-bottom">9</p>
+        <p class="text-secondary border-bottom">hello, ...?</p>
+        <p class="text-secondary border-bottom">seen</p>
+        <p class="text-secondary border-bottom">24-02-2019</p>
+            <a class="btn btn-sm btn-success" href="http://localhost/admin-panel/comment/approved/1">click to approved</a>
+            <a class="btn btn-sm btn-warning" href="http://localhost/admin-panel/comment/approved/1">click to not approved</a>
+    </section>
+</section>
+
