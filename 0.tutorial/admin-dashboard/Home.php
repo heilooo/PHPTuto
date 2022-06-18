cbc67f4c4b8244f81545f144494f10f6f059c2b9
abbasDadvand <am.dadvand@gmail.com>
2022-06-18 20:28:10 +0430

    0.12.0*65

--- 0.tutorial/admin-dashboard/Home.php
+++ 0.tutorial/admin-dashboard/Home.php
@@ -54,12 +54,35 @@ class Home
 
     public function category($id)
     {
+        $db = new DataBase();
+        $category = $db->select("SELECT * FROM `categories` WHERE `id` = ? ORDER BY `id` DESC ;", [$id])->fetch();
+        $articles = $db->select("SELECT articles.*, (SELECT COUNT(*) FROM comments WHERE comments.article_id = articles.id) AS comments_count, (SELECT username FROM users WHERE users.id = articles.user_id) AS username FROM articles WHERE (articles.cat_id = ?) ;", [$id])->fetchAll();
 
+        $popularArticles = $db->select("SELECT articles.*, (SELECT COUNT(*) FROM comments WHERE comments.article_id = articles.id) AS comments_count, (SELECT username FROM users WHERE users.id = articles.user_id) AS username FROM articles  ORDER BY `view` DESC LIMIT 0,4 ;")->fetchAll();
+        $sidebarPopularArticles = $popularArticles;
+        $categories = $db->select('SELECT * FROM `categories` ORDER BY `id` DESC ;');
+        $menus = $db->select('SELECT *, (SELECT COUNT(*) FROM `menus` AS `submenus` WHERE `submenus`.`parent_id` = `menus`.`id`  ) as `submenu_count`  FROM `menus` WHERE `parent_id` IS NULL ;')->fetchAll();
+        $submenus = $db->select('SELECT * FROM `menus` WHERE `parent_id` IS NOT NULL ;')->fetchAll();
+        $setting= $db->select("SELECT * FROM `websetting`;")->fetch();
+
+
+
+        require_once (realpath(dirname(__FILE__) . "/../template/app/show-category.php"));
     }
 
     public function commentStore($request)
     {
-
+        if(isset($_SESSION['user'])){
+            if($_SESSION['user'] != null){
+                $db= new DataBase();
+                $db->insert('comments', ['user_id', 'article_id', 'comment'], [$_SESSION['user'], $request['article_id'], $request['comment']]);
+                $this->redirectBack();
+            }
+            else
+                $this->redirectBack();
+        }
+        else
+            $this->redirectBack();
     }
 
     public function redirectBack()
